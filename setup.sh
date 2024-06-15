2f709b29f517bef1ff983f8593ca1105de724f09
Ryan Lucas <1649129+rdlucas2@users.noreply.github.com>
2024-06-15 04:56:57 +0000

    setup for my mediaserver

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Create directories
-sudo mkdir -p /srv/sabnzbd/{config,downloads,incomplete/downloads}
-sudo mkdir -p /srv/jellyfin/{config,cache}
-sudo mkdir -p /srv/jellyseerr/config
-sudo mkdir -p /srv/radarr/config
-sudo mkdir -p /srv/sonarr/config
-sudo mkdir -p /srv/lidarr/config
-sudo mkdir -p /srv/readarr/config
+sudo mkdir -p /home/ryan/srv/sabnzbd/{config,downloads,incomplete/downloads}
+sudo mkdir -p /home/ryan/srv/jellyfin/{config,cache}
+sudo mkdir -p /home/ryan/srv/jellyseerr/config
+sudo mkdir -p /home/ryan/srv/radarr/config
+sudo mkdir -p /home/ryan/srv/sonarr/config
+sudo mkdir -p /home/ryan/srv/lidarr/config
+sudo mkdir -p /home/ryan/srv/readarr/config
 
 # ensure that /etc/fstab is setup to mount your external storage drive (unless using other storage)
 # /etc/fstab
